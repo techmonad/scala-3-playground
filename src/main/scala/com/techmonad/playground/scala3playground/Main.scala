94e2c8f7a36d95e05f4157f9a3ba093b66d6d53e
Anand Singh <imanandksingh@gmail.com>
2021-07-13 21:48:09 +0200

    Init Commit

---
+++ src/main/scala/com/techmonad/playground/scala3playground/Main.scala
@@ -0,0 +1,9 @@
+package com.techmonad.playground
+package scala3playground
+
+object Main extends App:
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
---
+++ src/test/scala/com/techmonad/playground/scala3playground/ExampleSuite.scala
@@ -0,0 +1,7 @@
+package com.techmonad.playground
+package scala3playground
+
+final class ExampleSuite extends TestSuite:
+  test("hello world") {
+    1 `shouldBe` 1
+  }
