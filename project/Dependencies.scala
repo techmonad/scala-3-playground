096ed8538b9b3c4931c04ef0ca1918d275ed051a
Anand Singh <imanandksingh@gmail.com>
2022-09-26 10:16:47 +0200

    Update scalatest from 3.2.12 to 3.2.13 (#49)
    Co-authored-by: Anand Singh <4903911+anand-singh@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   case object org {
     case object scalatest {
       val scalatest =
-        "org.scalatest" %% "scalatest" % "3.2.12"
+        "org.scalatest" %% "scalatest" % "3.2.13"
     }
 
     case object scalatestplus {
